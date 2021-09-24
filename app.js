b0054e2007a07357db3a082dbffed9a17fdc5b63
leonardopad <77343517+leonardopad@users.noreply.github.com>
2021-09-24 20:52:13 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function trocaImagem(){
+    let escolha = document.getElementById('escolhaJogador').value;
+    let img = document.getElementById('imagemEscolha')
+    if(escolha == 'pedra'){
+       img.setAttribute('src', 'tesoura.png' ) 
+    }
+
+}
+function comparaVencedor() {
+    let escolha = document.getElementById('escolhaJogador').value;
+   
+    
+}
